b3f5a029b7258e78fc6c3f2c6c9771f6f5a03c04
greatabel <greatabel1@126.com>
2016-08-26 17:06:53 +0800

    change size of up_down iamge 2

--- static/js/memo_measure.js
+++ static/js/memo_measure.js
@@ -124,7 +124,8 @@ function update(touches) {
   var nh = window.innerHeight;
   if ((w != nw) || (h != nh)) {
     w = nw ;
-    h = nh - 125;
+    // h = nh - 125;
+    h = nh - 150;
     canvas.style.width = w+'px';
     canvas.style.height = h+'px';
     canvas.width = w;
