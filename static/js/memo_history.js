6d9a83de8dc6cafce727bc1e9947d9b221f282d5
greatabel <greatabel1@126.com>
2016-08-31 23:31:16 +0800

    fix 127.0.0.1 bug

--- static/js/memo_history.js
+++ static/js/memo_history.js
@@ -1,5 +1,5 @@
 var api_url = "http://139.224.73.50"
-api_url = "http://127.0.0.1:5000"
+// api_url = "http://127.0.0.1:5000"
 
 $.getJSON(api_url + '/api/user/0/measures', function(data) {
     children = ['梦遥','冬冬']
