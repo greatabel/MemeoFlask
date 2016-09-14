0dfce85f55409bf25ba9ca981a5c08ad6a67dede
greatabel <greatabel1@126.com>
2016-09-14 11:52:29 +0800

    modify sql

--- db.sql
+++ db.sql
@@ -57,6 +57,8 @@ insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('255,151,152,1
 insert Patient_User(patientid, userid, createdate) values(0,0,now());
 insert Patient_User(patientid, userid, createdate) values(1,0,now());
 insert Patient_User(patientid, userid, createdate) values(2,1,now());
+insert  Patient(name,sex,birthday, picture,createdate) values('小明',0,now(),LOAD_FILE('/Users/wanchang/Downloads/AbelProject/MeomoFlask/Abc.jpg'), (now() - INTERVAL 2 YEAR)  );
+insert  Patient(name,sex,birthday, picture,createdate) values('小红',1,now(),LOAD_FILE('/Users/wanchang/Downloads/AbelProject/MeomoFlask/Abc.jpg'), (now() - INTERVAL 3 YEAR)  );
 
 */
 
