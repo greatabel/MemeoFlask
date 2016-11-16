9425bbbc378054dd494321e63faafd78bf16dbb7
greatabel <greatabel1@126.com>
2016-11-16 16:15:40 +0800

    change data 2

--- temp_fix.sql
+++ temp_fix.sql
@@ -30,9 +30,9 @@ insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-90',1, false
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-100',1, false, now() - interval 6 day);
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-110',1, false, now() - interval 7 day);
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-120',1, false, now() - interval 8 day);
-insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-130',1, false, now() - interval 9 day);
-insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-140',1, false, now() - interval 10 day);
-insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-130',1, false, now() - interval 11 day);
+insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-70',1, false, now() - interval 9 day);
+insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-60',1, false, now() - interval 10 day);
+insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-50',1, false, now() - interval 11 day);
 
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-20',1, true, now() - interval 1 day);
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-30',1, true, now() - interval 2 day);
@@ -45,5 +45,5 @@ insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-40',1, true,
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-50',1, true, now() - interval 9 day);
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-60',1, true, now() - interval 10 day);
 insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-70',1, true, now() - interval 11 day);
-insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-80',1, true, now() - interval 12 day);
+insert MeasureRaw(rawdata, patientid, whicheye,createdate) values('-50',1, true, now() - interval 12 day);
 
