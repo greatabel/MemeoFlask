524ea2b38a187a299967799d549703d4bdcb38df
greatabel <greatabel1@126.com>
2016-12-22 15:24:23 +0800

    script sql

--- azure.sql
+++ azure.sql
@@ -6,6 +6,7 @@ truncate table  users;
 SET FOREIGN_KEY_CHECKS = 1;
 
 insert into users(`name`,`birthday`,`created_date`) VALUES('putao_abel',now(),now());
+insert into userinfos(`name`,`user_id`) values('test_info',1);
 insert patients(name,sex,birthday,created_date) values('小明',0,now(),(now() - INTERVAL 2 YEAR)  );
 insert patients(name,sex,birthday,created_date) values('小红',1,now(),(now() - INTERVAL 2 YEAR)  );
 -- insert patients(name,sex,birthday,created_date) values('小明',0,now(),(now() - INTERVAL 2 YEAR)  );
